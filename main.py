fa1c209fe66188860597cbefe1c2a7084b33b954
cscintia <csermakcintia14@gmail.com>
2021-01-16 14:51:59 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
-class WalkingSession:
-    
-    def __init__(self):
-        self.x = []    # creates a new empty list
-        self.y = []    # creates a new empty list
-        self.z = []    # creates a new empty list
-        self.mag = []    # creates a new empty list
-        self.mean = 0
-        self.minPeakHeight = 0
-        self.magNoG = []    # creates a new empty list
-        self.peaks = []    # creates a new empty list
-        self.steps = 0
-
-    def add_x(self, x):
-        self.x.append(x)
-        
-    def add_y(self, y):
-        self.y.append(y)
-        
-    def add_z(self, z):
-        self.z.append(z)
-    
-    def myfunc(void):
-        print("Hello")
-        
 from sense_hat import SenseHat
 from time import sleep
+from datetime import datetime
 from math import sqrt
 from numpy import std
 from scipy.signal import find_peaks
+
+class SamplePoint:
+    def __init__(self, x, y, z):
+        self.x = x * 9.8
+        self.y = y * 9.8
+        self.z = z * 9.8
+        self.mag = 0
+        self.magNoG = 0
+     
+class WalkingSession:
+    
+    def __init__(self):
+        self.samplepoints = [] # creates a new empty list
+        self.mean = 0
+        self.minPeakHeight = 0
+        self.peaks = []    # creates a new empty list
+        self.steps = 0
+        self.type =  0
+        self.starttime = 0
+        self.stoptime = 0
+        self.duration = 0
+        self.calories = 0        
+
 sense = SenseHat()
 
 e = (0, 0, 0)
@@ -38,25 +37,24 @@ sense.clear()
 sessions = []
 NumSessions = 0
 
-
 state = 0
 
 while True:
     if(state == 1):
-  
+        # getting data from accelerometer
         acceleration = sense.get_accelerometer_raw()
         x = acceleration['x']
         y = acceleration['y']
         z = acceleration['z']
-        sessions[NumSessions-1].add_x(x*9.8)
-        sessions[NumSessions-1].add_y(y*9.8)
-        sessions[NumSessions-1].add_z(z*9.8)
+        
+        #appending a new SamplePoint to the WalkingSession
+        sessions[NumSessions-1].samplepoints.append(SamplePoint(x, y, z))
   
     for event in sense.stick.get_events():
-    # Check if the joystick was pressed
+    # Check whether the joystick was pressed
         if event.action == "pressed":
         
-            # Check which direction
+            # Checking direction
             if event.direction == "up":
                 sense.show_letter("U")      # Up arrow
             elif event.direction == "down":
@@ -69,53 +67,41 @@ while True:
                 sense.show_letter("M")      # Enter key
                 if(state == 1):
                     state = 0
-                    #print(sessions[NumSessions-1].x)
-                    #print(sessions[NumSessions-1].y)
-                    #print(sessions[NumSessions-1].z)
                     
-                    for (i,j,k) in zip(sessions[NumSessions-1].x, sessions[NumSessions-1].y, sessions[NumSessions-1].z):
-                        sessions[NumSessions-1].mag.append(sqrt(i * i + j * j + k * k))
+                    for i in sessions[NumSessions-1].samplepoints:
+                        i.mag = sqrt(i.x * i.x + i.y * i.y + i.z * i.z)
                     
-                    #print(sessions[NumSessions-1].mag)
+                    summa = 0
+                    num = 0
+                    for i in sessions[NumSessions-1].samplepoints:
+                        num = num + 1
+                        summa = summa + i.mag
                     
-                    summ = 0
-                    numb = 0
-                    for i in sessions[NumSessions-1].mag:
-                        numb = numb + 1
-                        summ = summ + i
+                    sessions[NumSessions-1].mean = summa / num
                     
-                    sessions[NumSessions-1].mean = summ / numb
-                    print(sessions[NumSessions-1].mean)
+                    magNoGArray = []
+                    for i in sessions[NumSessions-1].samplepoints:
+                        i.magNoG = i.mag - sessions[NumSessions-1].mean
+                        magNoGArray.append(i.magNoG)
                     
-                    for i in sessions[NumSessions-1].mag:
-                        sessions[NumSessions-1].magNoG.append(i - sessions[NumSessions-1].mean)
-                    #print(sessions[NumSessions-1].magNoG)
+                    sessions[NumSessions-1].minPeakHeight = std(magNoGArray)
                     
-                    sessions[NumSessions-1].minPeakHeight = std(sessions[NumSessions-1].magNoG)
-                    print(sessions[NumSessions-1].minPeakHeight)
-                    
-                    #for i in sessions[NumSessions-1].magNoG:
-                    #    if i >= sessions[NumSessions-1].minPeakHeight:
-                    #        sessions[NumSessions-1].peaks.append(sessions[NumSessions-1].magNoG)
-                    #        sessions[NumSessions-1].steps = sessions[NumSessions-1].steps + 1
-                    
-                    sessions[NumSessions-1].peaks, _ = find_peaks(sessions[NumSessions-1].magNoG, height=sessions[NumSessions-1].minPeakHeight, distance=50)
+                    sessions[NumSessions-1].peaks, _ = find_peaks(magNoGArray, height=sessions[NumSessions-1].minPeakHeight, distance=50)
                     
                     print(sessions[NumSessions-1].peaks)
                     sessions[NumSessions-1].steps = sessions[NumSessions-1].peaks.size
                     print(sessions[NumSessions-1].steps)
+                    
+                    sessions[NumSessions-1].stoptime = datetime.now()
+                    sessions[NumSessions-1].duration = sessions[NumSessions-1].stoptime.timestamp() - sessions[NumSessions-1].starttime.timestamp()
+                    print("duration =", sessions[NumSessions-1].duration)
                     
                 else:
                     state = 1
                     
                     NumSessions += 1;
                     sessions.append(WalkingSession())
-                    
-                    
-                    
-                    sessions[NumSessions-1].myfunc()
-
-                    #print(NumSessions)
+                    sessions[NumSessions-1].starttime = datetime.now()
         
         # Wait a while and then clear the screen
         sleep(0.5)
