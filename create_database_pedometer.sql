b3442f76903917e092c416f7762dc57c7a0c3bc3
cscintia <csermakcintia14@gmail.com>
2021-01-16 17:26:49 +0100

    db creation sql script

---
+++ create_database_pedometer.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS pedometer;
+CREATE DATABASE pedometer;
+USE pedometer;
+CREATE TABLE WalkingSession(
+    ID INT NOT NULL AUTO_INCREMENT,
+    Type CHAR(1) NOT NULL,
+    Mean DOUBLE NOT NULL,
+    StartTime DATETIME NOT NULL,
+    StopTime DATETIME NOT NULL,
+    Duration DOUBLE NOT NULL,
+    CountOfSteps INT NOT NULL,
+    Calories DOUBLE NOT NULL,
+    
+    PRIMARY KEY (ID)
+	   
+);
